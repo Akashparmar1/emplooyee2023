a7143035cc0db93cfc2e21e7c776d6bf6bd8ecd7
Akashparmar1 <98416929+Akashparmar1@users.noreply.github.com>
2023-11-27 00:34:20 +0530

    E

--- src/app/page.js
+++ src/app/page.js
@@ -121,7 +121,7 @@ const Home = () => {
               <div className=" px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-28">
                 <div className="mr-auto place-self-center lg:col-span-7">
                   <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
-                    Welcome To employee Management System. 
+                    Welcome To Employee Management System. 
                   </h1>
                   <p className="max-w-2xl mb-6 font-light text-gray-300 lg:mb-8 md:text-lg lg:text-xl ">
                       Scalable | 
