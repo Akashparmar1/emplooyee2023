f2bb4288e8da4d750a3b8da8e21399af8248d4c9
Akashparmar1 <98416929+Akashparmar1@users.noreply.github.com>
2023-11-27 10:39:22 +0530

    hellloio

--- src/app/page.js
+++ src/app/page.js
@@ -116,7 +116,7 @@ const Home = () => {
               </form>
             </div>
           </div>
-          <div className="hidden lg:block lg:w-1/2 content bg-themeColor" style={{ background: "linear-gradient(to right, #41295a, #2f0743)"}}>
+          <div className="hidden lg:block lg:w-1/2 content bg-themeColor" style={{ background: "linear-gradient(90deg, rgba(66,91,247,1) 25%, rgba(62,11,75,0.918732458803834) 71%);"}}>
             <section className="bg-transparent">
               <div className=" px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-28">
                 <div className="mr-auto place-self-center lg:col-span-7">
